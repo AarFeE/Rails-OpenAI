7922c6f44dffb531623d973776082e6428450368
AarFeE <andres.jimenezv@hotmail.com>
2024-12-10 04:21:22 -0500

    Fix JS using Turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,16 +3,19 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "flowbite"
 
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("turbo:load", function() {
     const select = document.getElementById('show-mail-select');
     const mail = document.getElementById('mail-opt');
-  
-    select.addEventListener('change', function() {
-      console.log("THE VALUE IS: " + select.value);
-      if (select.value === 'true') {
-        mail.style.display = 'block';
-      } else {
-        mail.style.display = 'none';
-      }
-    });
+
+    if (select) {
+        select.addEventListener('change', function() {
+            console.log("THE VALUE IS: " + select.value);
+            if (select.value === 'true') {
+                mail.style.display = 'block';
+            } else {
+                mail.style.display = 'none';
+            }
+        });
+    }
 });
